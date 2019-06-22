d2a3cf4cae3595b1a20749b507001ed2c9873251
sharadg <91043+sharadg@users.noreply.github.com>
2019-06-22 09:48:08 -0500

    adding caching for gradle builds

--- ci/build-artifact.sh
+++ ci/build-artifact.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+export ROOT_FOLDER=$(pwd)
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+GRADLE_HOME="${HOME}/.gradle"
+GRADLE_CACHE="${ROOT_FOLDER}/gradle"
+ 
+echo "Generating symbolic links for caches"
+ 
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+[[ -d "${GRADLE_CACHE}" && ! -d "${GRADLE_HOME}" ]] && ln -s "${GRADLE_CACHE}" "${GRADLE_HOME}"
+
 cd git-repo
 ./gradlew build
 cp build/libs/*.jar ../artifact-dir/${base_name}.jar
